99f108969b84111d33d6e72aaedf0e7fa47603e4
anushka830 <116376569+anushka830@users.noreply.github.com>
2023-09-18 12:16:53 +0530

    Create Water consumption.cpp

---
+++ Water consumption.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X>=2000){
+	        cout<<"YES\n";
+	    } else{
+	        cout<<"NO\n";
+	    }
+	}
+	return 0;
+}
