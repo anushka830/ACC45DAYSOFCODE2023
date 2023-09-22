e00524015492e8cf4a05194e03eb013d9aac7438
anushka830 <116376569+anushka830@users.noreply.github.com>
2023-09-22 09:35:42 +0530

    Create Day-5 (Beginner) Battery Low.cpp

---
+++ Day-5 (Beginner) Battery Low.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x<=15){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
