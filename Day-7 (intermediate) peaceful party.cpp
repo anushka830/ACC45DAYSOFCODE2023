fd9476bd8904f07bd6d6d486a9bfe4f16f56cb46
anushka830 <116376569+anushka830@users.noreply.github.com>
2023-09-24 15:49:20 +0530

    Create Day-7 (intermediate) peaceful party.cpp

---
+++ Day-7 (intermediate) peaceful party.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t>0){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if(a+c>b){
+	        cout<<a+c<<endl;
+	    }
+	    else{
+	        cout<<b<<endl;
+	    }
+	    t--;
+	}
+	return 0;
+}
