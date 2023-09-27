371aa69198446bc0ff935f8258cb93bd6a9746d2
anushka830 <116376569+anushka830@users.noreply.github.com>
2023-09-27 08:58:39 +0530

    Create Day-10(beginner) Valentine.cpp

---
+++ Day-10(beginner) Valentine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	        cout<<x/y<<endl;
+	}  
+	return 0;
+}
