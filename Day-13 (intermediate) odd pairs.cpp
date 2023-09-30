63a9fb1338bf2bf1d7aa5654e52f6b16d6ed0561
anushka830 <116376569+anushka830@users.noreply.github.com>
2023-09-30 14:37:15 +0530

    Create Day-13 (intermediate) odd pairs.cpp

---
+++ Day-13 (intermediate) odd pairs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long n;
+	    cin>>n;
+	    cout<<(n*n)/2<<endl;
+	}
+	return 0;
+}
