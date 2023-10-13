4da659e4f442af2576a2341f21065c7b7a6448af
anushka830 <116376569+anushka830@users.noreply.github.com>
2023-10-13 19:31:59 +0530

    Create Day-26 Ludo.cpp

---
+++ Day-26 Ludo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x==6){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
