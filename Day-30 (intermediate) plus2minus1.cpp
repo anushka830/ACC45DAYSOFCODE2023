235ebab90286d79197ac6320113d1aee557d54cd
ANUSHKA SHARMA <116376569+anushka830@users.noreply.github.com>
2023-10-17 20:09:35 +0530

    Create Day-30 (intermediate) plus2minus1.cpp

---
+++ Day-30 (intermediate) plus2minus1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int y;
+	    cin>>y; 
+	    if(y==0){
+	        cout<<1<<endl;
+	    }
+	    else{
+	        cout<<y*3<<endl;
+	    }
+	}
+	return 0;
+}
